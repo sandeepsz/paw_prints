2bc6069938906c54261bd641c8ec22bbaf95dbd4
sandip <sandeep47nepali@gmail.com>
2024-06-27 15:43:33 +0545

    add about section

--- components/About.tsx
+++ components/About.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <div className="w-[1150px]  md:flexBetween lg:gap-28 mx-20 md:mx-12 mt-20 md:mt-28 lg:mt-8">
+        <div>
+          <h3>WHO WE ARE AND OUR MISSION</h3>
+        </div>
+        <div></div>
+
+        <div></div>
+      </div>
+    </>
+  );
 };
 
 export default About;
